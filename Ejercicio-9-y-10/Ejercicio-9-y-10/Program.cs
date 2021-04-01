96a47c503831dab0687f665cd6b46d3729c26608
demialko <demialko@gmail.com>
2021-04-01 17:15:01 -0300

    Finalización de ejercicios 9 y 10

---
+++ Ejercicio-9-y-10/Ejercicio-9-y-10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_9_y_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra = "";
+            do
+            {
+                Console.WriteLine("Ingrese una frase");
+                palabra = Console.ReadLine();
+            } while (palabra == "");
+            Console.WriteLine("La frase en minuscula es " + palabra.ToLower());
+            Console.WriteLine("La frase en mayuscula es " + palabra.ToUpper());
+            Console.ReadKey();
+        }
+    }
+}
